3e5950359b2f641592d10c1251f484ac01b5963a
Filip Copija <Filip.Copija@microsoft.wsei.edu.pl>
2022-07-07 15:59:33 +0200

    ADDED: Helper.cs to improve connection way

---
+++ WPF_App/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_App
+{
+    internal class Helper
+    {
+        public static string connection = @"Data Source=DESKTOP-FOQ5J3H;Initial Catalog=Magazyn;Integrated Security=True";
+    }
+}
